df5c282d3f62b07a6b49ea8b36331a9998f464cd
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2023-08-18 14:54:57 +0200

    Update logger formatting, add dev script

--- logger.ts
+++ logger.ts
@@ -3,11 +3,22 @@ import { isDenoDeploy } from "./utils.ts";
 
 log.setup({
   handlers: {
-    console: new log.handlers.ConsoleHandler("DEBUG"),
+    console: new log.handlers.ConsoleHandler("DEBUG", {
+      formatter: (logRecord) => {
+        let msg = `${logRecord.levelName} ${logRecord.msg}`;
+
+        logRecord.args.forEach((arg, index) => {
+          msg += `, arg${index}: ${arg}`;
+        });
+
+        return msg;
+      },
+    }),
   },
   loggers: {
     default: {
       level: isDenoDeploy ? "INFO" : "DEBUG",
+      handlers: ["console"],
     },
   },
 });
